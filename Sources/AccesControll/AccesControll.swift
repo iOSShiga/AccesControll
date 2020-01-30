7cc94cb8fe690f329a68de91e4d5e826583931e9
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-01-30 23:04:10 +0530

    aadd

--- Sources/AccesControll/AccesControll.swift
+++ Sources/AccesControll/AccesControll.swift
@@ -1,3 +1,19 @@
 struct AccesControll {
     var text = "Hello, World!"
 }
+
+
+open class OpenClass {
+   open var name:String = "Open Class"
+    open func getOpenClass() {
+        print("Open Fun")
+    }
+}
+
+public class PublicClass {
+    public var name:String = "Public Class"
+    public func getPublicClass(){
+        print("Publi Class")
+    }
+}
+
